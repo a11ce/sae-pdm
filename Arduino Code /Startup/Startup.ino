8aca684f0b372ed8cc064b258d0b56cf1694b3b6
a11ce <a11ce@protonmail.com>
2021-04-28 19:47:34 -0500

    index-based cutoff (way better)

--- Arduino Code /Startup/Startup.ino
+++ Arduino Code /Startup/Startup.ino
@@ -1,7 +1,7 @@
 #define TEST_MODE
 
 #ifdef TEST_MODE
-  // TESTING LED VALUES - COMMENT OUT IN REAL USE
+  // TESTING LED VALUES 
   // CUT 1
   const int SPEED_SENSE_OUT = 14;
   const int TEMP_SENSE_OUT = 15;
@@ -26,42 +26,29 @@
   const int COOLANT_PUMP_OUT = 43;
 #endif
 
-
-
-
-
 #define BATTERY_IN A6
 #define VOLT_MULT 4;
 
-
-
-
 #define CUT1_MIN 11
-int cut1Outs[] = {SPEED_SENSE_OUT, TEMP_SENSE_OUT,
-                    POT_SENSE_OUT, BRAKE_PRESSURE_OUT};
-int cut1Outs_INV[] = {RAD_FAN_OUT, COOLANT_PUMP_OUT, 
-                      BRAKELIGHT_OUT};
-
 #define CUT2_MIN 10.5
-int cut2Outs[] = {RAD_FAN_OUT, BRAKELIGHT_OUT};
-int cut2Outs_INV[] = {COOLANT_PUMP_OUT};
+#define CUT3_MIN 10
 
-#define ALLOUTS_MIN 10
-int allOuts[] = {RAD_FAN_OUT, COOLANT_PUMP_OUT, 
-                 BRAKELIGHT_OUT,SPEED_SENSE_OUT,
-                 TEMP_SENSE_OUT, POT_SENSE_OUT,
-                 BRAKE_PRESSURE_OUT};
+// everything at or below the given index will be cut
+// if the voltage drops below the threshold;
+// read it as 'the first N in the array will stay on'
+#define CUT1_IDX 3 // no sensors
+#define CUT2_IDX 1 // just the coolant pump
+#define CUT3_IDX 0 // everything off
+
+int allOuts[] = {COOLANT_PUMP_OUT, 
+
+                RAD_FAN_OUT, BRAKELIGHT_OUT,
+
+                SPEED_SENSE_OUT, TEMP_SENSE_OUT,
+                POT_SENSE_OUT, BRAKE_PRESSURE_OUT};
+
 
 #define ALLOUTS_LEN sizeof(allOuts) / sizeof(int)
-#define CUT1OUTS_LEN sizeof(cut1Outs) / sizeof(int)
-#define CUT2OUTS_LEN sizeof(cut1Outs) / sizeof(int)
-
-#define CUT1OUTS_INV_LEN sizeof(cut1Outs_INV) / sizeof(int)
-#define CUT2OUTS_INV_LEN sizeof(cut1Outs_INV) / sizeof(int)
-
-
-
-
 
 
 float batteryVoltage() { 
@@ -71,21 +58,22 @@ float batteryVoltage() {
   return voltage;
 }
 
+void setCutoff(int cutoffIdx)
+{
+  for (int i = 0; i < ALLOUTS_LEN; i++)
+  {
+    digitalWrite(allOuts[i], i > cutoffIdx ? HIGH : LOW);
+  }
+  
+}
+
 void setup() {
   
   for (int i = 0; i < ALLOUTS_LEN; i++)
   {
     pinMode(allOuts[i], OUTPUT);
   }
-}
-
-void writeArrOuts(int val, int arr[], int len)
-{
-  for (int i = 0; i < len; i++)
-  {
-    digitalWrite(arr[i], val);
-  }
-  
+  setCutoff(-1);
 }
 
 
@@ -93,25 +81,21 @@ void loop() {
 
   float battery = batteryVoltage();
    
-  if(battery < ALLOUTS_MIN)
+  if(battery < CUT3_MIN)
   {
-    writeArrOuts(LOW, allOuts, ALLOUTS_LEN);
+    setCutoff(CUT3_IDX);
   }
   else if (battery < CUT2_MIN)
   {
-    // cut everything
-    writeArrOuts(LOW, allOuts, ALLOUTS_LEN);
-    // except coolant pump
-    digitalWrite(COOLANT_PUMP_OUT, HIGH);
+    setCutoff(CUT2_IDX);
   }
-  else if (battery < CUT1_MIN)
+    else if (battery < CUT1_MIN)
   {
-    writeArrOuts(LOW, cut1Outs, CUT1OUTS_LEN);
-    writeArrOuts(HIGH, cut1Outs_INV, CUT1OUTS_INV_LEN);
+    setCutoff(CUT1_IDX);
   }
   else
   {
-    writeArrOuts(HIGH, allOuts, ALLOUTS_LEN);
+    setCutoff(-1);
   }
 
 }
