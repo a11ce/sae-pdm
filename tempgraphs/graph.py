a45ba6b0a864a8ff3de8352c9df2e270d3d6840a
riley <a11ce@protonmail.com>
2021-05-26 23:55:48 -0500

    20210526 brake temp (#2)
    * Modified the code to run FL & FR brake temps
    
    * Added better commenting
    
    Co-authored-by: thomasmyers <83887690+thomasmyers@users.noreply.github.com>

--- tempgraphs/graph.py
+++ tempgraphs/graph.py
@@ -1,12 +1,26 @@
 import PySimpleGUI as sg
 import time
 
-RANGES = {"temp": (5, 45)}
-RANGES.update({"accel" + d: (-4, 4) for d in "xy"})
-RANGES.update({"accelz": (-12, -8)})
-RANGES.update({"mag" + d: (-100, 100) for d in "xyz"})
-RANGES.update({"gyro" + d: (-4, +4) for d in "xyz"})
+#RANGES = {"temp": (5, 45)}
+#RANGES.update({"accel" + d: (-4, 4) for d in "xy"})
+#RANGES.update({"accelz": (-12, -8)})
+#RANGES.update({"mag" + d: (-100, 100) for d in "xyz"})
+#RANGES.update({"gyro" + d: (-4, +4) for d in "xyz"})
 
+RANGES = {"Front left brake temp (C)": (5, 45)}
+RANGES.update({"Front right brake temp (C)": (5, 45)})
+RANGES.update({"accel" + d + " (m/s^2)": (-4, 4) for d in "xy"})
+RANGES.update({"accel z (m/s^2)": (-12, 2)})
+RANGES.update({"gyro (rad/s)" + d: (-4, +4) for d in "xyz"})
+
+#the next line defines which portions of the piped input string are considered FL brake temp, FR brake temp, etc
+usefulindexes = [4,5,24,25,26,27,28,29]
+#as written, the 4th element is FL brake temp, the 5th is FR brake temp, the 24th is accel x, the 25th is accel y
+#the 26th is accel z, the 27th is gyro x, 28th gyro y and 29th gyro z
+#everything else in the string is discarded
+#this may need a little tweaking to match the output of main.py
+#the string is separated by spaces, so "1 2 3 4" becomes [1,2,3,4]
+ 
 ORDERED_KEYS = list(RANGES.keys())
 
 HISTDAT = {key: [] for key in ORDERED_KEYS}
@@ -24,13 +38,17 @@ def makeGraph(gKey, gRange):
 
 
 def makeWindow():
-    controlSection = [sg.Text("look its graphs")]
+    print(ORDERED_KEYS)
+    print(RANGES)
+    print(ORDERED_KEYS[0:2])
+    print(ORDERED_KEYS[2:5])
+    print(ORDERED_KEYS[5:8])
+    controlSection = [sg.Text("30 most recent data points from telemetry")]
 
-    layout = [[makeGraph(ORDERED_KEYS[0], RANGES[ORDERED_KEYS[0]])] +
+    layout = [[makeGraph(key, RANGES[key]) for key in ORDERED_KEYS[0:2]] +
               controlSection,
-              [makeGraph(key, RANGES[key]) for key in ORDERED_KEYS[1:4]],
-              [makeGraph(key, RANGES[key]) for key in ORDERED_KEYS[4:7]],
-              [makeGraph(key, RANGES[key]) for key in ORDERED_KEYS[7:10]]]
+              [makeGraph(key, RANGES[key]) for key in ORDERED_KEYS[2:5]],
+              [makeGraph(key, RANGES[key]) for key in ORDERED_KEYS[5:8]]]
 
     window = sg.Window("window title", layout, finalize=True)
     return window
@@ -61,6 +79,7 @@ def updateGraphs(window):
 
 def main():
     window = makeWindow()
+    print(window)
     initGraphs(window)
     fpsCount = 0
     start_time = time.time()
@@ -70,10 +89,14 @@ def main():
         if event == sg.WIN_CLOSED or event == 'Exit':
             break
         inLine = input().strip().split(" ")
+        valuesused = 0
         for valIdx, val in enumerate(inLine[1:]):
-            HISTDAT[ORDERED_KEYS[valIdx]].append(float(val))
-            HISTDAT[ORDERED_KEYS[valIdx]] = HISTDAT[
-                ORDERED_KEYS[valIdx]][-30::]
+            if valIdx in usefulindexes:
+                HISTDAT[ORDERED_KEYS[valuesused]].append(float(val))
+                HISTDAT[ORDERED_KEYS[valuesused]] = HISTDAT[
+                    ORDERED_KEYS[valuesused]][-30::]
+                valuesused += 1
+                    
         #print(inLine)
         updateGraphs(window)
 
--- tempgraphs/testdat.py
+++ tempgraphs/testdat.py
@@ -2,8 +2,21 @@ import random
 import time
 import sys
 
+tenbrackets = "{} {} {} {} {} {} {} {} {} {} "
+
 while True:
-    print("10VALS {} {} {} {} {} {} {} {} {} {}".format(
+    print(("35VALS " + tenbrackets + tenbrackets + tenbrackets + "{} {} {} {} {}").format(
+        random.randint(0, 10), #0
+        random.randint(0, 10), #1
+        random.randint(0, 10), #2
+        random.randint(5, 45), #3
+        random.randint(5, 45), #FL brake temp
+        random.randint(5, 20), #FR brake temp
+        random.randint(0, 10),
+        random.randint(0, 10),
+        random.randint(0, 10),
+        random.uniform(0, 1),
+        
         random.randint(0, 10),
         random.randint(0, 10),
         random.randint(0, 10),
@@ -14,6 +27,24 @@ while True:
         random.randint(0, 10),
         random.randint(0, 10),
         random.uniform(0, 1),
+        
+        random.randint(0, 10), #20
+        random.randint(0, 10), #21
+        random.randint(0, 10), #22
+        random.randint(0, 10), #23
+        random.randint(0, 10), #accelx
+        random.randint(0, 1),  #accely
+        -random.randint(8, 12), #accelz
+        -random.randint(0, 1), #gyrox
+        -random.randint(0, 2), #gyroy
+        random.uniform(0, 1),  #gyroz
+        
+        random.randint(0, 10),
+        random.randint(0, 10),
+        random.randint(0, 10),
+        random.randint(0, 10),
+        random.randint(0, 10),
+        
     ))
     sys.stdout.flush()
     time.sleep(0.05)
